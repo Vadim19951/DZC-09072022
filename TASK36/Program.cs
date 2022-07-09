79116ab4a2afddcaba7465f636fc83abdab11a82
Vadim <uvolkov@374@gmail.com>
2022-07-09 20:23:42 +0300

    Add Task36

---
+++ TASK36/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] array = new int[8];
+
+int sum = 0;
+
+FillArray(array);
+PrinArray(array);
+
+for(int i = 0; i < array.Length; i++)
+{
+    if(i % 2 != 0)
+    {
+        sum = sum + array[i];
+    }
+} 
+Console.WriteLine();
+Console.WriteLine("Сумма чисел на нечетных позициях в массиве равна " + sum);
+
+
+void PrinArray(int[]array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+void FillArray(int[]array)

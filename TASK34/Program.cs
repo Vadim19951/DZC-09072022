d8aa3f4e56ee7d039bab79a0ca742d0fc2f96d7a
Vadim <uvolkov@374@gmail.com>
2022-07-09 20:17:47 +0300

    Add task34

---
+++ TASK34/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] array = new int[8];
+
+int count = 0;
+
+FillArray(array);
+PrinArray(array);
+
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 == 0)
+    {
+        count++;
+    }
+} 
+Console.WriteLine("Колличество четных чисел в массиве равно " + count);
+
+
+void PrinArray(int[]array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+void FillArray(int[]array)
+{
+for(int i = 0; i < array.Length; i++ )
+{
+     array[i] = new Random().Next(99, 1000);
+}
+}
